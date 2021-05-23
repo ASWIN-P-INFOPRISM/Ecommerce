f48537e7ef341e42ba60210e4ce9d93ae8a1e09c
Aswin <aswinpalapetty@gmail.com>
2021-05-23 21:57:40 +0530

    data from database displayed in admin page

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,10 +1,22 @@
 var db = require('../config/connection')
+var collection=require('../config/collection-names')
+
 
 module.exports={
     productHelp : (product,callback)=>{
-        db.get().collection('product').insertOne(product).then((data)=>{
+        db.get().collection(collection.PRODUCT).insertOne(product).then((data)=>{
             console.log(data)
             callback(data.ops[0]._id)
         })
+    },
+    
+ productDisplay : ()=>{
+        return new Promise(async(resolve,reject)=>{
+
+           let products = await db.get().collection(collection.PRODUCT).find().toArray()
+           resolve(products)
+
+        })
+            
+        }
     }
-}
