a5b16377ccf919584007e423c7780d36fe887e10
Aswin <aswinpalapetty@gmail.com>
2021-05-24 13:13:43 +0530

    user login checked in console,signup made successful

--- helpers/account-helpers.js
+++ helpers/account-helpers.js
@@ -1,11 +1,35 @@
 var db = require('../config/connection')
 var collection=require('../config/collection-names')
-
+const bcrypt = require('bcrypt')
 
 module.exports={
-    ofLogin : (details)=>{
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collection.USER_DATA).insertOne(details)
+    ofSignup : (details)=>{
+        return new Promise(async(resolve,reject)=>{
+            details.password = await bcrypt.hash(details.password,10)
+            db.get().collection(collection.USER_DATA).insertOne(details).then((data)=>{
+                
+                resolve(data.ops[0])
+
+            })
         })
+    },
+    ofLogin :(details)=>{
+        return new Promise(async(resolve,reject)=>{
+            let user= await db.get().collection(collection.USER_DATA).findOne({email : details.email})
+            if(user){
+                bcrypt.compare(details.password,user.password).then((status)=>{
+                    if(status){
+                        console.log('LOGINED');
+                    }
+                    else{
+                        console.log('LOGIN UNSUCCESSFULL');
+                    }
+                })
+            }
+            else{
+                console.log('LOGIN UNSUCCESSFULL');
+            }
+        })
+
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var productHelpers= require('../helpers/product-helpers')
-
+var productHelpers = require('../helpers/product-helpers')
+var accountHelpers = require('../helpers/account-helpers')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  
-  productHelpers.productDisplay().then((products)=>{
+router.get('/', function (req, res, next) {
+
+  productHelpers.productDisplay().then((products) => {
     console.log(products)
-    res.render('user/view-product', { title: 'E-Commerce', products});
+    res.render('user/view-product', { title: 'E-Commerce', products });
   });
 
 });
 
 
-router.get('/signup',(req,res)=>{
+router.get('/signup', (req, res) => {
   res.render('user/signup');
 });
 
-router.post('/signup',(req,res)=>{
+router.post('/signup', (req, res) => {
 
-  
-  
+  accountHelpers.ofSignup(req.body).then((data) => {
+    console.log(data)
+    res.render('user/login')
+  })
+
 })
 
 
 
 
 
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
   res.render('user/login');
 });
 
-router.post('/login',(req,res)=>{
-  
-})
+router.post('/login', (req, res) => {
+
+  accountHelpers.ofLogin(req.body);
+
+});
 
 module.exports = router;
